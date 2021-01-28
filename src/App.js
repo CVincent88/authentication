d7e9e6cd5dfc1a0167cec86c5d31fcd1f97fec86
CVincent88 <come.vincent@live.fr>
2021-01-28 14:52:49 +0100

    Installed night mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./Components/GlobalStyle";
+import { lightTheme, darkTheme } from "./Components/Themes"
 
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles/>
+        <div className="App">
+          <button onClick={themeToggler}>Switch Theme</button>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
